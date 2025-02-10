049dd719222cb609f9259fe1dd95e8a72df65168
hannahchapman79 <hannahchapman1999@aol.com>
2025-02-10 17:05:26 +0700

    Adds routes to create project skeleton

---
+++ app/admin/events/add/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return ( 
+        <>
+        <h1>Add new event form</h1>
+        </>
+    )
+}
+
+export default Page;
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return ( 
+        <>
+        <h1>Login Form</h1>
+        </>
+    )
+}
+
+export default Page;

3c3db153010d58b2d2850e05fd9933ee3b274114
hannahchapman79 <hannahchapman1999@aol.com>
2025-02-17 19:41:41 +0700

    Adds basic loading component

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <p className="text-lg font-semibold text-primary animate-pulse">
+        Loading...
+      </p>
+    </div>
+  );
+}
